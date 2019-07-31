14f6bd82151567ec769e09b165bd60e7468f3128
Aathiganesh <aathi93ganesh@gmail.com>
2019-07-31 23:01:22 +0530

    First commit

--- Design Pattern/src/Strategy_Pattern/Duck.java
+++ Design Pattern/src/Strategy_Pattern/Duck.java
@@ -4,7 +4,7 @@ public abstract class Duck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("aathi");
+		System.out.println("aathi--");
 	}
 
 }
