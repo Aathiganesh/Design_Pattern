0bec625b14c29ff0c61d334bac62e137c11ac844
Aathiganesh <aathi93ganesh@gmail.com>
2019-08-02 23:02:34 +0530

    Adding more observers

--- Design Pattern/src/Observer_Pattern/Simulator.java
+++ Design Pattern/src/Observer_Pattern/Simulator.java
@@ -4,8 +4,11 @@ public class Simulator {
 	public static void main (String args[]) {
 		SimpleSubject ss = new SimpleSubject();
 		SimpleObseerver so = new SimpleObseerver(ss);
-		
+		ss.setvalue(14);
+		SimpleObseerver so1 = new SimpleObseerver(ss);
+		SimpleObseerver so2 = new SimpleObseerver(ss);
 		ss.setvalue(11);
+		
 		
 	}
 }
